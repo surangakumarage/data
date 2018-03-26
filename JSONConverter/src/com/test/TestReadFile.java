ee3790fbf1ebd9a72df3f332ea88eb9d235b8f9c
suranga <suranga@SURANGA-W10DT.codegen.net>
2018-03-26 17:50:58 +0530

    read file

---
+++ JSONConverter/src/com/test/TestReadFile.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.util.ReadFile;
+
+public class TestReadFile {
+	
+	public static void main (String[] args){
+		ReadFile read = new ReadFile();
+		read.readFile("C:\\Users\\suranga\\git\\data\\JSONConverter\\resources\\Customer.csv");
+
+	}
+
+}
--- JSONConverter/src/com/util/ReadFile.java
+++ JSONConverter/src/com/util/ReadFile.java
@@ -1,5 +1,51 @@
 package com.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadFile {
+	
+	public void readFile(String fileName){
+
+
+			BufferedReader br = null;
+			FileReader fr = null;
+
+			try {
+
+				fr = new FileReader(fileName);
+				br = new BufferedReader(fr);
+
+				String sCurrentLine;
+
+				while ((sCurrentLine = br.readLine()) != null) {
+					System.out.println(sCurrentLine);
+				}
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+
+			} finally {
+
+				try {
+
+					if (br != null)
+						br.close();
+
+					if (fr != null)
+						fr.close();
+
+				} catch (IOException ex) {
+
+					ex.printStackTrace();
+
+				}
+
+			}
+
+		
+	}
 
 }
